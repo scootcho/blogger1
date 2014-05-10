37da72e9ee1e5b79e234d164656584e48496e5d3
Scott Yu <scott.tj.yu@gmail.com>
2014-05-10 10:06:37 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Blogger tutorial from JumpStartLab
+=================================
+http://tutorials.jumpstartlab.com/projects/blogger.html#blogger-2
+
+Create a simple blog system and learn the basics of Ruby on Rails including:
+
+*Models, Views, and Controllers (MVC)
+*Data Structures & Relationships
+*Routing
+*Migrations
+*Views with forms, partials, and helpers
+*RESTful design
+*Adding gems for extra features
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '4ef13a2fbb2da94fbf7060d33d1d83294ba8ca750dd515743b235607cb4b537d4911dcb650e350ef7dee1c500b16f8050d1ae33e949d221a65557af8e44438ea'
